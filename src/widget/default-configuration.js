7a506307e8e2d92b1791939836abeb4453074e59
Trần Duy Quang <51506787+tranduyquang2205@users.noreply.github.com>
2023-04-24 05:27:27 +0700

    1

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -1,15 +1,14 @@
 
 export const defaultConfiguration = {
-    titleClosed: 'Click to chat!',
-    titleOpen: 'Let\'s chat!',
+    titleClosed: 'Hỗ trợ!',
+    titleOpen: 'Hỗ trợ!',
     closedStyle: 'chat', // button or chat
     closedChatAvatarUrl: '', // only used if closedStyle is set to 'chat'
     cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
-    introMessage: 'Hello! How can we help you?',
-    autoResponse: 'Looking for the first available admin (It might take a minute)',
-    autoNoResponse: 'It seems that no one is available to answer right now. Please tell us how we can ' +
-    'contact you, and we will get back to you as soon as we can.',
-    placeholderText: 'Send a message...',
+    introMessage: 'Chào mừng bạn đến với Babygroup.club! Bạn cần hỗ trợ gì?',
+    autoResponse: 'Đang sắp xếp tư vấn viên hỗ trợ bạn...',
+    autoNoResponse: 'Có vẻ như tất cả các tư vấn viên đều đang bận, bạn vui lòng thử lại sau nhé! Xin lỗi vì sự bất tiện này',
+    placeholderText: 'Gửi tin nhắn...',
     displayMessageTime: true,
     mainColor: '#1f8ceb',
     alwaysUseFloatingButton: false,
