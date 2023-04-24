8eb58d95b7e02e22bd48c1c36d91514b97b89bd4
Trần Duy Quang <51506787+tranduyquang2205@users.noreply.github.com>
2023-04-24 07:03:30 +0700

    1

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/hook', function(req, res){
                 "Markdown");
         } else if (reply) {
             let replyText = reply.text || "";
-            let userId = replyText.split(':')[0];
+            let userId = replyText.split('SĐT:')[0].replace('ID: ','');
             io.to(userId).emit(chatId + "-" + userId, {name, text, from: 'admin'});
         } else if (text){
             io.emit(chatId, {name, text, from: 'admin'});
